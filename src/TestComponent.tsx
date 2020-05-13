3da4178fc8d3dafacc3776d449cd41d9ec477f96
Travis Hilterbrand <Travis.Hilterbrand@five9.com>
2020-05-13 12:35:38 -0700

    Add enum example

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+export enum TestComponentsVariants {
+  primary = 'primary',
+  secondary = 'secondary',
+};
+
 export interface onTestComponentChangeEvent {
   value: string
 };
@@ -19,25 +24,38 @@ interface TestComponentProps {
    */
   value:string
   /**
+   * variant of the component
+   */
+  variant: TestComponentsVariants
+  /**
    * Triggered when user changes value
    * @param {onTestComponentChangeEvent} e - Changed value
    */
   onChange: onTestComponentChange
 };
+const TestComponentDefaultProps = {
+  id: null,
+  label: null,
+  variant: TestComponentsVariants.primary,
+  value: null,
+  onChange: function() {},
+};
 
 const TestComponent = (props: TestComponentProps) => {
-  const { id, label, value, onChange } = props;
+  const { id, label, value, variant, onChange } = props;
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (typeof onChange === 'function') {
       onChange({value: e.currentTarget.value});
     }
   };
+  const rootClass = ['root', variant].join(' ');
   return (
-    <div>
+    <div className={rootClass}>
       <label htmlFor={id}>{label}:</label>
       <input id={id} autoComplete={'off'} value={value} onChange={handleChange} />
     </div>
   );
 };
+TestComponent.defaultProps = TestComponentDefaultProps;
 
 export default TestComponent;
